75f33a1af9594921aac21ea182399b5903aed6a0
elephant10 <besevavv@gmail.com>
2023-09-23 10:58:09 +0300

    removed moderator

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ try {
 const bot = new Telegraf(apiKey)
 
 const authorizedUserIds = [
-  497250936, 622436187
+  497250936 //, 622436187
 ]; // Array of authorized user IDs
 
 // let conversationEnded = true;
