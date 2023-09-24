4ce2b7eb2e69ff63f916fdd2b3aeb000555b87f6
ulliss2015 <hosting-fafnir@website.drygdryg.lds.net.ua>
2023-09-24 17:23:48 +0300

    Push file users.json userIds.txt ban.txt

--- git-sync.sh
+++ git-sync.sh
@@ -4,14 +4,19 @@
 REPO_URL="https://github.com/elephant10/Bla-Bla-Club-suggestion-bot.git"
 
 # Коммит-сообщение
-COMMIT_MESSAGE="Выгрузка файлов users.json, userIds.txt и ban.txt"
+COMMIT_MESSAGE="Push files users.json, userIds.txt и ban.txt"
 
-# Добавляем изменения в индекс Git
-git add .
+# Список файлов, которые нужно выгрузить
+FILES_TO_UPLOAD=("users.json" "userIds.txt" "ban.txt")
+
+# Добавляем указанные файлы в индекс Git
+for file in "${FILES_TO_UPLOAD[@]}"; do
+    git add "$file"
+done
 
 # Создаем коммит
 git commit -m "$COMMIT_MESSAGE"
 
-# Отправляем изменения на сервер (предполагается, что у вас настроена аутентификация)
-git push origin main  # Замените "main" на вашу актуальную ветку
+# Отправляем изменения на сервер
+git push origin main
 
